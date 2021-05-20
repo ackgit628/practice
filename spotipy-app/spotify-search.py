23686c3d621c8941ecbadedcb2b262f7be8599b1
Akhilesh Hessa <akhilesh4us@gmail.com>
2021-05-21 03:13:18 +0530

    Added scope for user-top-read

--- spotipy-app/spotify-search.py
+++ spotipy-app/spotify-search.py
@@ -7,10 +7,10 @@ from json.decoder import JSONDecodeError
 from spotipy.oauth2 import SpotifyOAuth
 
 # Create the spotify object
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR CLIENT ID",
-                                               client_secret="YOUR SECRET ID",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="MY CLIENT ID",
+                                               client_secret="MY SECRET ID",
                                                redirect_uri="YOUR REDIRECT URI",
-                                               scope="user-library-read"))
+                                               scope="user-library-read user-top-read"))
 
 # get the current user
 user = sp.current_user()
@@ -24,8 +24,11 @@ while True:
     print()
     print("1 Search for a track")
     print("2 Search for an artist")
+    print("3 View your top tracks")
+    print("4 View your top artists")
     print("9 Exit")
 
+    print()
     choice = input("Your choice: ")
 
     # search for a track
@@ -54,7 +57,7 @@ while True:
 
         trackChoice = input("Enter the track number you searched for (x to exit): ")
         if trackChoice == "x":
-            break
+            continue
 
         # Select Track and Display info
         trackInfo = sp.track(trackIDs[int(trackChoice)])
@@ -117,6 +120,48 @@ while True:
             if songSelection == "x":
                 break
             webbrowser.open(trackArt[int(songSelection)])
+
+    # get current user's top tracks
+    if choice == "3":
+        results = sp.current_user_top_tracks(10,0,'long_term')
+        
+        # Album Art
+        trackArt = []
+
+        print()
+        for idx, item in enumerate(results['items']):
+            print(idx, item['artists'][0]['name'], " – ", item['name'])
+            albumArt = item['album']['images'][0]['url']
+            trackArt.append(albumArt)
+        print()
+
+        # See album art
+        while True:
+            songSelection = input("Enter a song number to see its album art (x to exit): ")
+            if songSelection == "x":
+                break
+            webbrowser.open(trackArt[int(songSelection)])
+
+    # get current user's top artists
+    if choice == "4":
+        results = sp.current_user_top_artists(10,0,'long_term')
+        
+        # Artist image
+        artistImage = []
+
+        print()
+        for idx, item in enumerate(results['items']):
+            print(idx, item['name'], "\t\t", item['followers']['total'], "followers")
+            image = item['images'][0]['url']
+            artistImage.append(image)
+        print()
+
+        # See album art
+        while True:
+            artistSelection = input("Enter a number to view the artist's image (x to exit): ")
+            if artistSelection == "x":
+                break
+            webbrowser.open(artistImage[int(artistSelection)])
 
     # exit spotipy
     if choice == "9":
